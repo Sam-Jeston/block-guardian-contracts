150379553e2ec71268c95411a825b7a035206013
Sam Jeston <sam.jeston@gmail.com>
2023-10-03 22:52:21 +1000

    feat: simplify contract to only store 32 byte proofs
    Also cleaned up the repo and README

--- README.md
+++ README.md
@@ -1,12 +1,20 @@
 # Block Guardian Verifier Contracts
 
-## Design Considerations / TODOs
-- Add a toll if the caller of the program is not the "toll free" account. This will let us expose a nice UX for users to pay for their own transactions, while we still make a small amount of $. The toll free account will be used for us handling txs for web2 users
+The purpose of this contract is to allow users to store 32 bytes of arbitrary data on-chain, recording the timestamp the data was persisted and who persisted it. This contract is designed to solve auditibilty issues faced by existing web2 companies. There are a number of circumstances where you want to keep your data private, but also want to be able to prove that a file has existed in its current form since a certain date. This contract allows you to do that by either submitting SHA256 hashes directly to the contract, or by instead storing the root of a merkle tree.
 
-## Anchor
+Some real-world examples of when this is useful:
+
+- When a company is audited, they can present the hashes of their financial records to the auditor. This allows the auditor to verify that the financial records have not been tampered with.
+- While creating custom IP, the creator can store the hashes of their files on-chain. This allows the creator to prove that has the IP in their possession at a certain date. This does not prove that they created the IP, but in the event someone claims to have created it after that time, the creator can prove that they had the IP first.
+- During evidence collection, a number of photos / documents / media files are collected. These files are then hashed and the hashes are stored on-chain. This allows the investigating party to prove that the files have not been tampered with since the time of collection.
+- When making insurance claims, any files submitted that support ownership / state of property prior to the claim can be verified by the insurance company. This allows the insurance company to have confidence that the files submitted are not fraudulent.
+
+## Development
+
+### Interacting with the program
+
+The tests in `tests/block-guardian-verifier.ts` show how to interact with the program.
+
+### Run the Tests
+
 `anchor test`
-## Anchor Tutorial
-https://lorisleiva.com/create-a-solana-dapp-from-scratch/testing-our-instruction
-
-## Merkle Trees
-Using open zepplin merkle tree library
--- tests/block-guardian-verifier.ts
+++ tests/block-guardian-verifier.ts
@@ -1,197 +1,108 @@
-import * as anchor from "@coral-xyz/anchor";
-import { Program } from "@coral-xyz/anchor";
-import { BlockGuardianVerifier } from "../target/types/block_guardian_verifier";
-import { StandardMerkleTree } from "@openzeppelin/merkle-tree";
-import { PublicKey } from '@solana/web3.js'
+import * as anchor from '@coral-xyz/anchor'
+import { Program } from '@coral-xyz/anchor'
+import { BlockGuardianVerifier } from '../target/types/block_guardian_verifier'
+import { StandardMerkleTree } from '@openzeppelin/merkle-tree'
+import { expect } from 'chai'
 
-const ADMIN_ADDRESS = '2sN2GNKZHroHZ1TZMBJPS16R4xjKyXzcFvt8nBapjddA'
-
-describe("block-guardian-verifier", () => {
-  // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
+describe('block-guardian-verifier', () => {
+  anchor.setProvider(anchor.AnchorProvider.env())
 
   const program = anchor.workspace
-    .BlockGuardianVerifier as Program<BlockGuardianVerifier>;
+    .BlockGuardianVerifier as Program<BlockGuardianVerifier>
 
-  it("can create a new proof", async () => {
-    // (1)
-    const values = [["car"], ["case"], ["bat"], ["ball"], ["foo"], ["lee"]];
+  // This test is a good example of how the contract can be used to store a proof
+  // for multiple files by storing the root of a merkle tree instead of a sha256 hash directly.
+  // The tree shored be stored in immutable, publicly accessible storage somewhere off-chain so that
+  // the files can always be verified
+  it('can store a merkle tree root on-chain', async () => {
+    const fileHashes = [
+      ['0x27b506fb70a825eeee33b167e5995ce050b609bb8718863cfe17c9cfb98fcacd'],
+      ['0x4089caf31ddd6d66ecd13ded600af070f179b5a8292eb2ffac2237cecd4953db'],
+      ['0xa2d2510f195d4f8985e19718f1f28fbe9247a79f6716caaa7ab90b2af4788bab'],
+    ]
 
-    // (2)
-    const tree = StandardMerkleTree.of(values, ["string"]);
+    const tree = StandardMerkleTree.of(fileHashes, ['string'])
 
-    // (3)
-    console.log("Merkle Root:", tree.root);
-
-    const verified = StandardMerkleTree.verify(
-      tree.root,
-      ["string"],
-      ["ball"],
-      tree.getProof(3)
-    );
-    console.log(tree.getProof(3))
-    console.log(JSON.stringify(tree.dump(), null, 2));
-    console.log({ verified });
-
-    const initialAdminBlanace = await program.provider.connection.getBalance(new PublicKey(ADMIN_ADDRESS))
-    console.log(initialAdminBlanace.toString())
-
-    const proofKeypair = anchor.web3.Keypair.generate();
+    const proofKeypair = anchor.web3.Keypair.generate()
     await program.methods
       .storeProof(hexToUint8Array(tree.root))
       .accounts({
         proofAccount: proofKeypair.publicKey,
-        adminAccount: new PublicKey(ADMIN_ADDRESS),
-        // creator: anchorProvider.wallet.publicKey,
-        // systemProgram: anchor.web3.SystemProgram.programId,
       })
       .signers([proofKeypair])
-      .rpc();
-  
-    // Fetch the account details of the created tweet.
+      .rpc()
+
+    // Show that the proof account was created and how it can be queried
+    // on-chain
     const proofAccounts = await program.account.proof.all([
       {
-        dataSize: 81, // number of bytes of the expected account
+        dataSize: 80, // number of bytes of the expected account
       },
       {
         memcmp: {
-          offset: 8, // Discriminator.
+          offset: 8, // 8 byte offset for the discriminator.
           bytes: hexToBase58(tree.root),
-        }
-      }
-    ]);
-    console.log({ proofAccount: '0x' + Buffer.from(proofAccounts[0].account.proof).toString('hex') });
-    console.log(JSON.stringify(proofAccounts[0], null, 2))
-    const postAdminBalance = await program.provider.connection.getBalance(new PublicKey(ADMIN_ADDRESS))
-    console.log(postAdminBalance.toString())
+        },
+      },
+    ])
 
-
-    const noProof = await program.account.proof.all([
-      {
-        memcmp: {
-          offset: 8, // Discriminator.
-          bytes: hexToBase58('0xe4faa4a13f8f3562287edfad7e089f7c7ef0135c00190e6d8772eab67dad61f7'),
-        }
-      }
-    ]);
-
-    console.log({noProof})
-  });
-
-  it("fails if a bad admin account is sent", async () => {
-    // (1)
-    const values = [["car"], ["case"], ["bat"], ["ball"], ["foo"], ["lee"]];
-
-    // (2)
-    const tree = StandardMerkleTree.of(values, ["string"]);
-
-    // (3)
-    console.log("Merkle Root:", tree.root);
-
+    const rootVerifiedOnChain =
+      '0x' + Buffer.from(proofAccounts[0].account.proof).toString('hex')
     const verified = StandardMerkleTree.verify(
-      tree.root,
-      ["string"],
-      ["ball"],
-      tree.getProof(5)
-    );
-    console.log(tree.dump());
-    console.log({ verified });
+      rootVerifiedOnChain,
+      ['string'],
+      ['0x4089caf31ddd6d66ecd13ded600af070f179b5a8292eb2ffac2237cecd4953db'],
+      // This tree object can be rebuilt by the tree stored off-chain on Immutable storage
+      tree.getProof(1)
+    )
 
-    const initialAdminBlanace = await program.provider.connection.getBalance(new PublicKey(ADMIN_ADDRESS))
-    console.log(initialAdminBlanace.toString())
+    expect(verified).to.be.true
+  })
 
-    const proofKeypair = anchor.web3.Keypair.generate();
+  // This is a very interesting consequence that I didn't expect. When parsing in dynamiclly sized data
+  // to program methods (i.e String), if persisting the data you need to validate the size of the data.
+  // When exposing a method that excepts a byte array of a fixed size, contract validation does nothing
+  // as the size of the data is guaranteed to be the fixed size. I probably would have expected anchor to
+  // potentially throw from the client code in this instance
+  it('if the proof is more than 32 bytes, the first 32 bytes are persisted as the proof', async () => {
+    const validHash =
+      '0xa2d2510f195d4f8985e19718f1f28fbe9247a79f6716caaa7ab90b2af4788bab'
+    const oversizedHash = `${validHash}ababab`
+
+    const proofKeypair = anchor.web3.Keypair.generate()
     await program.methods
-      .storeProof(hexToUint8Array(tree.root))
+      .storeProof(hexToUint8Array(oversizedHash))
       .accounts({
         proofAccount: proofKeypair.publicKey,
-        adminAccount: new PublicKey('3sN2GNKZHroHZ1TZMBJPS16R4xjKyXzcFvt8nBapjddA'),
-        // creator: anchorProvider.wallet.publicKey,
-        // systemProgram: anchor.web3.SystemProgram.programId,
       })
       .signers([proofKeypair])
-      .rpc();
-  
-    // Fetch the account details of the created tweet.
+      .rpc()
+
     const proofAccounts = await program.account.proof.all([
       {
-        dataSize: 81, // number of bytes of the expected account
+        dataSize: 80, // number of bytes of the expected account
       },
       {
         memcmp: {
-          offset: 8, // Discriminator.
-          bytes: hexToBase58(tree.root),
-        }
-      }
-    ]);
-    console.log({ proofAccount: '0x' + Buffer.from(proofAccounts[0].account.proof).toString('hex') });
-    console.log(JSON.stringify(proofAccounts[0], null, 2))
-    const postAdminBalance = await program.provider.connection.getBalance(new PublicKey(ADMIN_ADDRESS))
-    console.log(postAdminBalance.toString())
+          offset: 8, // 8 byte offset for the discriminator.
+          bytes: hexToBase58(validHash),
+        },
+      },
+    ])
 
+    const hashVerifiedOnChain =
+      '0x' + Buffer.from(proofAccounts[0].account.proof).toString('hex')
 
-    const noProof = await program.account.proof.all([
-      {
-        memcmp: {
-          offset: 8, // Discriminator.
-          bytes: hexToBase58('0xe4faa4a13f8f3562287edfad7e089f7c7ef0135c00190e6d8772eab67dad61f7'),
-        }
-      }
-    ]);
-
-    console.log({noProof})
-  });
-
-  // it('can send a new tweet from another author', async () => {
-  //     const otherUser = anchor.web3.Keypair.generate();
-  //     const signature = await program.provider.connection.requestAirdrop(otherUser.publicKey, 1000000000000)
-  //     await program.provider.connection.confirmTransaction(signature);
-
-  //     // Call the "SendTweet" instruction.
-  //     const tweet = anchor.web3.Keypair.generate();
-  //     await program.methods
-  //       .sendTweet('Hummus, am I right?')
-  //       .accounts({
-  //         tweet: tweet.publicKey,
-  //         author: otherUser.publicKey,
-  //         systemProgram: anchor.web3.SystemProgram.programId,
-  //       })
-  //       .signers([otherUser, tweet])
-  //       .rpc()
-
-  //     // Fetch the account details of the created tweet.
-  //     const tweetAccount = await program.account.tweet.fetch(tweet.publicKey);
-  //     expect(tweetAccount.author.toBase58()).to.eq(otherUser.publicKey.toBase58());
-  //     expect(tweetAccount.content).to.eq('Hummus, am I right?');
-  //     expect(tweetAccount.timestamp).to.not.be.null;
-  // });
-
-  // it('cannot send a tweet with more than 280 chars', async () => {
-  //     // Call the "SendTweet" instruction.
-  //     try {
-  //       const tweet = anchor.web3.Keypair.generate();
-  //       const contentWith281Chars = 'x'.repeat(281)
-  //       await program.methods
-  //         .sendTweet(contentWith281Chars)
-  //         .accounts({
-  //           tweet: tweet.publicKey,
-  //           author: anchorProvider.wallet.publicKey,
-  //           systemProgram: anchor.web3.SystemProgram.programId,
-  //         })
-  //         .signers([tweet])
-  //         .rpc()
-  //     } catch (e) {
-  //       expect(e.error.errorMessage).to.eq('The provided content should be 280 characters long maximum.')
-  //       return
-  //     }
-
-  //     throw new Error('Should have failed');
-  // });
-});
+    expect(hashVerifiedOnChain).to.equal(validHash)
+  })
+})
 
 function hexToUint8Array(hexString): number[] {
   // Filter off 0x from the hex string
-  return hexString.match(/.{1,2}/g).filter(pair => pair !== '0x').map(byte => parseInt(byte, 16));
+  return hexString
+    .match(/.{1,2}/g)
+    .filter((pair) => pair !== '0x')
+    .map((byte) => parseInt(byte, 16))
 }
 
 function hexToBase58(hexString): string {
